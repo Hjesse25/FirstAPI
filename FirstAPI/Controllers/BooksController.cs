837fe5a9c296e95d108de5d67edf6b22a21a2b47
Jesse <196892153+Hjesse25@users.noreply.github.com>
2025-05-11 19:29:20 -0700

    First commit

---
+++ FirstAPI/Controllers/BooksController.cs
@@ -0,0 +1,66 @@
+﻿using FirstAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        static private List<Book> books = new List<Book>
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "The Great Gatsby",
+                Author = "F. Scott Fitzgerald",
+                YearPublished = 1925
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "To Kill a Mockingbird",
+                Author = "Harper Lee",
+                YearPublished = 1960
+            },
+            new Book
+            {
+                Id = 3,
+                Title = "1984",
+                Author = "George Orwell",
+                YearPublished = 1949
+            },
+            new Book
+            {
+                Id = 4,
+                Title = "Moby-Dick",
+                Author = "Herman Melville",
+                YearPublished = 1951
+            },
+            new Book
+            {
+                Id = 5,
+                Title = "The Hobbit",
+                Author = "J.R.R Tolken",
+                YearPublished = 1937
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Book>> GetBooks()
+        {
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBookById(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound();
+
+            return Ok(book);
+        }
+    }
+}
