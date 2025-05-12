7069b49c6a5c25fd2dea203f5095946970c7710c
Jesse <196892153+Hjesse25@users.noreply.github.com>
2025-05-12 13:02:00 -0700

    Implement crud on db

--- FirstAPI/Controllers/BooksController.cs
+++ FirstAPI/Controllers/BooksController.cs
@@ -1,6 +1,8 @@
-﻿using FirstAPI.Models;
+﻿using FirstAPI.Data;
+using FirstAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FirstAPI.Controllers
 {
@@ -8,55 +10,60 @@ namespace FirstAPI.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
-        static private List<Book> books = new List<Book>
+        //static private List<Book> books = new List<Book>
+        //{
+        //    new Book
+        //    {
+        //        Id = 1,
+        //        Title = "The Great Gatsby",
+        //        Author = "F. Scott Fitzgerald",
+        //        YearPublished = 1925
+        //    },
+        //    new Book
+        //    {
+        //        Id = 2,
+        //        Title = "To Kill a Mockingbird",
+        //        Author = "Harper Lee",
+        //        YearPublished = 1960
+        //    },
+        //    new Book
+        //    {
+        //        Id = 3,
+        //        Title = "1984",
+        //        Author = "George Orwell",
+        //        YearPublished = 1949
+        //    },
+        //    new Book
+        //    {
+        //        Id = 4,
+        //        Title = "Moby-Dick",
+        //        Author = "Herman Melville",
+        //        YearPublished = 1951
+        //    },
+        //    new Book
+        //    {
+        //        Id = 5,
+        //        Title = "The Hobbit",
+        //        Author = "J.R.R Tolken",
+        //        YearPublished = 1937
+        //    }
+        //};
+        private readonly FirstAPIContext _context;
+        public BooksController(FirstAPIContext context)
         {
-            new Book
-            {
-                Id = 1,
-                Title = "The Great Gatsby",
-                Author = "F. Scott Fitzgerald",
-                YearPublished = 1925
-            },
-            new Book
-            {
-                Id = 2,
-                Title = "To Kill a Mockingbird",
-                Author = "Harper Lee",
-                YearPublished = 1960
-            },
-            new Book
-            {
-                Id = 3,
-                Title = "1984",
-                Author = "George Orwell",
-                YearPublished = 1949
-            },
-            new Book
-            {
-                Id = 4,
-                Title = "Moby-Dick",
-                Author = "Herman Melville",
-                YearPublished = 1951
-            },
-            new Book
-            {
-                Id = 5,
-                Title = "The Hobbit",
-                Author = "J.R.R Tolken",
-                YearPublished = 1937
-            }
-        };
+            _context = context;
+        }
 
         [HttpGet]
-        public ActionResult<List<Book>> GetBooks()
+        public async Task<ActionResult<List<Book>>> GetBooks()
         {
-            return Ok(books);
+            return Ok(await _context.Books.ToListAsync());
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Book> GetBookById(int id)
+        public async Task<ActionResult<Book>> GetBookById(int id)
         {
-            var book = books.FirstOrDefault(x => x.Id == id);
+            var book = await _context.Books.FindAsync(id);
             if (book == null)
                 return NotFound();
 
@@ -64,19 +71,21 @@ namespace FirstAPI.Controllers
         }
 
         [HttpPost]
-        public ActionResult<Book> AddBook(Book newBook)
+        public async Task<ActionResult<Book>> AddBook(Book newBook)
         {
             if (newBook == null)
                 return BadRequest();
 
-            books.Add(newBook);
-            return CreatedAtAction(nameof(GetBookById), new {id=newBook.Id}, newBook);
+            _context.Books.Add(newBook);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBookById), new { id = newBook.Id }, newBook);
         }
 
         [HttpPut("{id}")]
-        public IActionResult UpdateBook(int id, Book updatedBook)
+        public async Task<IActionResult> UpdateBook(int id, Book updatedBook)
         {
-            var book = books.FirstOrDefault(x => x.Id == id);
+            var book = await _context.Books.FindAsync(id);
             if (book == null)
                 return NotFound();
 
@@ -85,17 +94,21 @@ namespace FirstAPI.Controllers
             book.Author = updatedBook.Author;
             book.YearPublished = updatedBook.YearPublished;
 
+            await _context.SaveChangesAsync();
+
             return NoContent();
         }
 
         [HttpDelete("{id}")]
-        public IActionResult DeleteBook(int id)
+        public async Task<IActionResult> DeleteBook(int id)
         {
-            var book = books.FirstOrDefault(x => x.Id == id);
+            var book = await _context.Books.FindAsync(id);
             if (book == null)
                 return NotFound();
 
-            books.Remove(book);
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+
             return NoContent();
         }
     }
